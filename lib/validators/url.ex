a3fc53ab767fe51185eaa50df3c695f107d4c272
Chris Jimison <cjimison@gmail.com>
2022-09-27 11:14:06 -0700

    opps

--- lib/validators/url.ex
+++ lib/validators/url.ex
@@ -71,7 +71,7 @@ defmodule EctoCommons.URLValidator do
   end
 
   defp do_validate_url(value, _parsed, :parsable) do
-    case URI.new(String.to_charlist(value)) do
+    case URI.new(value) do
       {:ok, _uri} -> :ok
       {:error, _msg} -> :error
     end
